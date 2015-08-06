db8de64a6298348e42798f0d3333da8e8dd188c8
jorgeluis11 <perez.jorge718@gmail.com>
2015-08-06 08:54:54 -0400

    cadidate questions added

--- static/js/controller/controller.js
+++ static/js/controller/controller.js
@@ -17,12 +17,33 @@ angular.module("starter")
 	});
 
 }]).controller('questionCtrl', ['$scope', '$routeParams', '$http', function ($scope, $routeParams, $http) {
+	//$($(".ace_identifier")[0]).html()
+
 	$scope.questions = [];
 	$scope.submitButton = false;
+
+	$scope.nameInserted = false;
+	$scope.name = "";
 
 	$http.get("/api/languages/"+$routeParams.languageSlug+"/"+$routeParams.examSlug).then(function(data){
 		$scope.questions = data.data;
 		$scope.submitButton = true;
 	});
 
-}]);
+}])
+.directive('ngEnter', function () {
+    return {
+    	restrict:"A",
+    	link:function (scope, element, attrs) {
+        element.bind("keydown keypress", function (event) {
+            if(event.which === 13) {
+                scope.$apply(function (){
+                    // scope.$eval(attrs.ngEnter);
+                    scope.nameInserted = true;
+                });
+                event.preventDefault();
+            }
+        });
+    }
+}
+});
